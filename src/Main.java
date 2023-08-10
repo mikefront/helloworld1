66ee766df9671ac2340e1ea70ff8b41c4b9e4793
mikefront <mike_front@mail.ru>
2023-08-10 15:17:40 +0500

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    //Задание1
+    public static void main(String[] args) {
+        var dog = 8.0;
+        System.out.println("dog-" + dog); {
+        }
+        var cat = 3.6;
+        System.out.println("cat-" + cat);{
+        }
+        var paper = 763789;
+        System.out.println("paper-" + paper);{
+        }
+
+    }
+}
