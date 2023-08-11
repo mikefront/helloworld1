986266952547d721227b75c086dc299e54527cb5
mikefront <mike_front@mail.ru>
2023-08-11 12:54:29 +0500

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     //Задание1
-    public static void main(String[] args) {
-        var dog = 8.0;
-        System.out.println("dog-" + dog); {
+    public static void main(String[] args){ System.out.println("Привет мир!");
+            var dog = 8.0;
+            System.out.println("dog-" + dog); {
         }
-        var cat = 3.6;
-        System.out.println("cat-" + cat);{
+            var cat = 3.6;
+            System.out.println("cat-" + cat);{
         }
-        var paper = 763789;
-        System.out.println("paper-" + paper);{
+            var paper = 763789;
+            System.out.println("paper-" + paper);{
         }
 
+        }
     }
-}
